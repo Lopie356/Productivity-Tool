87b4205b5d0faa24ea456898c6a56cf796aaea27
Prcie <prcielopie@gmail.com>
2025-02-18 23:55:02 +0530

    Save current changes before switching branches

---
+++ frontend/src/pages/Timer.js
@@ -0,0 +1,13 @@
+// src/pages/Timer.js
+import React from 'react';
+
+function Timer() {
+  return (
+    <div>
+      <h1>Timer Page</h1>
+      <p>Yahan aap apni session details dekh sakte hain.</p>
+    </div>
+  );
+}
+
+export default Timer;
